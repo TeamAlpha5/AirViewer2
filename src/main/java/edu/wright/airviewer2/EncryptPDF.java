52d65325271886e41f8258ab470d589ce75c20c6
RamDixitErra <101813041+RamDixitErra@users.noreply.github.com>
2022-04-18 20:01:44 -0400

    Update EncryptPDF.java

--- src/main/java/edu/wright/airviewer2/EncryptPDF.java
+++ src/main/java/edu/wright/airviewer2/EncryptPDF.java
@@ -5,23 +5,36 @@ import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.WriterProperties;
+import com.google.java.contract.Invariant;
+import com.google.java.contract.Requires;
+import com.google.java.contract.Ensures;
+
 /*
  * @author erraramdixit
  *
  */
+@Invariant("fileName!= null && fileName.length() > 0")
 public class EncryptPDF {
 
-	/*
-	 * THe below method encrypts the pdf and keeps them secure with a password
-	 * File created date 04/16/2022 no changes made after this date
-	 * any changes made to it please update the date and document the reason for the upgrade
-	 */
 	public EncryptPDF() {
 		// TODO Auto-generated constructor stub
+		
 	}
 
 	
-	public void encryptPdf(String fileName) throws Exception {
+	/**
+	 * @param fileName
+	 * @throws Exception
+	 */
+	
+	/*
+	 * THe below method file encrypts the pdf and keeps them secure with a password
+	 * File created date 04/16/2022 no changes made after this date
+	 * any changes made to it please update the date and document the reason for the upgrade
+	 */
+	@Requires("fileName != null && fileName.length() > 0")
+	@Ensures("result == true")
+	public boolean encryptPdf(String fileName) throws Exception {
    	 String OWNER_PASSWORD = "World";
        PdfDocument pdfDoc = new PdfDocument(
                new PdfReader(fileName),
@@ -34,5 +47,6 @@ public class EncryptPDF {
                        EncryptionConstants.ENCRYPTION_AES_128 | EncryptionConstants.DO_NOT_ENCRYPT_METADATA))
        );
        pdfDoc.close();
+       return true;
    }
 }
