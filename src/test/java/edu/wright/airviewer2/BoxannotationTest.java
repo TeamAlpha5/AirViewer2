5f65f9e38cb3b9d4ffb8da83a37ba0287d6e0608
deepika-gadiraju <101816151+deepika-gadiraju@users.noreply.github.com>
2022-04-21 01:44:12 -0400

    Update BoxannotationTest.java

--- src/test/java/edu/wright/airviewer2/BoxannotationTest.java
+++ src/test/java/edu/wright/airviewer2/BoxannotationTest.java
@@ -3,32 +3,38 @@ package edu.wright.airviewer2;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.*; 
+import java.io.File;
+
+/**
+ * @author DeepikaGadiraju
+ *
+ */
+public class BoxannotationTest {
+	// Here object will be initialized
+	static BoxAnnotationMaker box = null;
+
 	/**
-	 * @author DeepikaGadiraju 
-	 *
+	 * @throws Exception
 	 */
-	public class BoxannotationTest {
-		//Here object will be initialized
-		static BoxAnnotationMaker box=null;
-		/**
-		 * @throws Exception
-		 */
-		@BeforeAll
-		public static void setUp() throws Exception {
-			//Here pdf file path will be given to test the file
-			box = new BoxAnnotationMaker();
-		}
-		/**
-		 * @throws Exception
-		 */
-		@AfterAll
-		public static void tearDown() throws Exception {
-			//Here object will be initialized to null
-			box = null;
-		}
-		/**
+	@BeforeAll
+	public static void setUp() throws Exception {
+		// Here pdf file path will be given to test the file
+		box = new BoxAnnotationMaker();
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@AfterAll
+	public static void tearDown() throws Exception {
+		// Here object will be initialized to null
+		box = null;
+	}
+
+	/**
 		 * testing method
 		 */
 		@org.junit.jupiter.api.Test
@@ -40,14 +46,13 @@ import org.junit.jupiter.api.*;
 				/*
 				 * here a call to the method will take place
 				 */
-File f=new File("c:/Documents/test.pdf");
-PDDocument docob=PDDocument.load(f);
-				box.make(docob,{"1","150","50","150","150","Hello"});
-			} 
+                              File f=new File("c:/Documents/test.pdf");
+                              PDDocument docob=PDDocument.load(f);
+                              box.make(docob,{"1","150","50","150","150","Hello"});
+			}
 			catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-
-	} 
+}
