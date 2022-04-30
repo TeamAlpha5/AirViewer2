3843eaa153504e8b9aa38c3cc58124341dce49c4
deepika-gadiraju <101816151+deepika-gadiraju@users.noreply.github.com>
2022-04-30 01:44:16 -0400

    Update PPTConversion.java

--- src/main/java/edu/wright/airviewer2/PPTConversion.java
+++ src/main/java/edu/wright/airviewer2/PPTConversion.java
@@ -7,19 +7,44 @@ import com.aspose.pdf.Document;
 import com.aspose.pdf.SaveFormat;
 import com.google.java.contract.Ensures;
 
+/**
+ * @author Deepika Gadiraju
+ *
+ */
+/**
+ * The class PPTConversion is used for converting pdf to ppt with a constructor which takes a pdf file path.
+ * It contains pptConversion() method which is used for the conversion of pdf to ppt format.
+ */
 @Invariant("filePath!= null && filePath.length() > 0")
 public class PPTConversion {	
 	String filePath;
 	
+/*
+ * This is a constructor where it takes path of the pdf as a parameter
+ * @param filePath
+ * 
+ * 
+ */
 	@Requires("filePath != null && filePath.length() > 0")
 	public PPTConversion(String filePath) {
 		this.filePath = filePath;
 	}
-    
+/**
+* @param args
+* @throws IOException
+* @throws DocumentException
+*/    
 	public static void main(String[] args)
 	        throws IOException, DocumentException
 	    {
 	    }
+/**
+* This method is used for the conversion of pdf to ppt file format
+* @pre(filepath.length()>0)
+* @post(result==true)
+* @throws Exception
+* @throws NullPointerException
+*/	
 	@Requires("filePath != null && filePath.length() > 0")
 	@Ensures("result == true")
 	@SuppressWarnings("unused")
@@ -28,10 +53,6 @@ public class PPTConversion {
 		Document doc = new Document(filePath);
 		// Save resultant DOC file
 		doc.save(filePath+"-dd.Pptx", SaveFormat.Pptx);
-		return true;
-		
-		
-	}
-
-	
+		return true;		
+	}	
 }
